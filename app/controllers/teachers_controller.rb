6c643b4a3200b7375c48a5c49581eb827fe0c652
Ammar Ali <funnyguy118@gmail.com>
2019-03-13 16:02:05 -0400

    updated the teacher's controller and relevant erb files: index, show, new.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,14 +2,30 @@ class TeachersController < Sinatra::Base
 
   set :views, 'app/views/teachers'
   set :method_override, true
-  
+
 
   #read
   get '/teachers' do
-
+    @teachers = Teacher.all
+    erb :index
   end
 
   #create
+  get '/teachers/new' do
+    erb :new
+  end
+
+  post '/teachers' do
+    teachers = Teacher.find_or_create_by(name: params[:name])
+    redirect "/teachers/#{teachers.id}"
+  end
+
+  #show teachers detail
+  get '/teachers/:id' do
+    @teacher = Teacher.find(params[:id])
+    @students = @teacher.students
+    erb :show
+  end
 
 
 
